f828a35da11a7d11eca3a063c7c98517bce856bd
NhonAleph <nhon.nguyen@aleph-labs.com>
2020-02-22 13:37:18 +0700

    Updates

---
+++ precache-manifest.f39ac1471495b70ee2619de198158cf2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d619c32039f2e177e683d01e50d878d",
+    "url": "/kamereo/index.html"
+  },
+  {
+    "revision": "0916aec087f256e9f6f5",
+    "url": "/kamereo/static/css/main.22c27fb0.chunk.css"
+  },
+  {
+    "revision": "7755610650f6ad376641",
+    "url": "/kamereo/static/js/2.2a20b5b9.chunk.js"
+  },
+  {
+    "revision": "0916aec087f256e9f6f5",
+    "url": "/kamereo/static/js/main.6bb0d278.chunk.js"
+  },
+  {
+    "revision": "3be707829f8e9d58afed",
+    "url": "/kamereo/static/js/runtime-main.2cebfb46.js"
+  },
+  {
+    "revision": "ef2915c09893b486e5568f2990913bea",
+    "url": "/kamereo/static/media/collapsible-arrow.ef2915c0.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4261a73e53c5e0a7621b7e084eeffa4b.js"
+  "/kamereo/precache-manifest.f39ac1471495b70ee2619de198158cf2.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/kamereo/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
